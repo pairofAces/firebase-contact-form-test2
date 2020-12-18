8d65de1da3a400c45743390aa3637fe0d72f4780
pairofAces <karanchauhan16@gmail.com>
2020-12-17 19:21:29 -0500

    fixed app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-apiKey: "AIzaSyD6G7PoBXwd46HisQmy0iC2_-kLQju34fE",
-authDomain: "contactform-test2-dbf79.firebaseapp.com",
-projectId: "contactform-test2-dbf79",
-storageBucket: "contactform-test2-dbf79.appspot.com",
-messagingSenderId: "387758895145",
-appId: "1:387758895145:web:d6f064278f40fcdd082c38"
+    apiKey: "AIzaSyD6G7PoBXwd46HisQmy0iC2_-kLQju34fE",
+    authDomain: "contactform-test2-dbf79.firebaseapp.com",
+    projectId: "contactform-test2-dbf79",
+    storageBucket: "contactform-test2-dbf79.appspot.com",
+    messagingSenderId: "387758895145",
+    appId: "1:387758895145:web:d6f064278f40fcdd082c38"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -13,32 +13,30 @@ firebase.initializeApp(firebaseConfig);
 //create a reference contactInfo "collections"
 let contactInfo = firebase.database().ref("infos");
 
+// Listen for a submit
+document.querySelector(".contact-form").addEventListener("submit", submitForm);
 
-//Need to listen for a submit
-document.querySelector('.contact-form').addEventListener("submit",
- submitForm);
+function submitForm(e) {
+  e.preventDefault();
 
-function submitForm (e) {
-    e.preventDefaukt();
-    // console.log("This is working")
+  //   Get input Values
+  let name = document.querySelector(".name").value;
+  let email = document.querySelector(".email").value;
+  let message = document.querySelector(".message").value;
+  console.log(name, email, message);
 
-    //Now I need to get all of the elements
-    let name = document.querySelector('.name').value;
-    let email = document.querySelector('.email').value;
-    let message = document.querySelector('.message').value;
+  saveContactInfo(name, email, message);
 
-    console.log(name, email, message);
-
-    saveContactInfo(name, email, message); //aspirational code 
+  document.querySelector(".contact-form").reset();
 }
 
-// need to save infos to Firebase
+// Save infos to Firebase
 function saveContactInfo(name, email, message) {
-    let newContactInfo = contactInfo.push();
+  let newContactInfo = contactInfo.push();
 
-    newContactInfo.set({
-        name: name,
-        email: email,
-        message: message,
-    })
+  newContactInfo.set({
+    name: name,
+    email: email,
+    message: message,
+  });
 }
